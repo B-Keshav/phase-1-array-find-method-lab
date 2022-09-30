1782fe1a30ffb99ec410020bd9b8dbfdc590608f
Keshav Bhati <keshavbhati2000@gmail.com>
2022-09-30 11:35:08 -0400

    commit tree 1cd4267d0f7a367c1502cca2603b9de5288ab225

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+//code your solution here
+const record = [
+    { year: "2013", result: "N/A"},
+    { year: "2014", result: "L"},
+    { year: "2015", result: "W"},
+]
+
+function superbowlWin(record) {
+    let dub = record.find((arr) => arr.result === "W") 
+    //return (dub)
+    if(dub){
+        return dub.year
+    }
+    // else{
+    //     return undefined
+    // }
+
+}
